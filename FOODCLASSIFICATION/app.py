e9c43f1c89d0911423115ba699a2989a6465e16e
jakem <mazurkiewiczj@duq.edu>
2024-03-08 14:23:11 -0500

    Added dependencies

--- FOODCLASSIFICATION/app.py
+++ FOODCLASSIFICATION/app.py
@@ -38,3 +38,45 @@ def fetch_calories(prediction) -> str:
     except Exception as e:
         st.error("Can't able to fetch the Calories")
         print(e)
+
+def processed_img(img_path):
+    img = load_img(img_path, target_size=(224, 224, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
+
+
+def run():
+    st.title("Fruits🍍-Vegetable🍅 Classification")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250, 250))
+        st.image(img, use_column_width=False)
+        save_image_path = './upload_images/' + img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+
+        # if st.button("Predict"):
+        if img_file is not None:
+            result = processed_img(save_image_path)
+            print(result)
+            if result in vegetables:
+                st.info('**Category : Vegetables**')
+            else:
+                st.info('**Category : Fruit**')
+            st.success("**Predicted : " + result + '**')
+            cal = fetch_calories(result)
+            if cal:
+                st.warning('**' + cal + '(100 grams)**')
+
+
+if __name__ == '__main__':
+    run()
