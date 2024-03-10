efd97a0f1b903c61bb28485589e11991b1906141
jakem <mazurkiewiczj@duq.edu>
2024-03-10 16:33:11 -0400

    Added Comments

--- FOODCLASSIFICATION/app.py
+++ FOODCLASSIFICATION/app.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #Loading Saved model
-model = load_model('FV.h5')
+model = load_model(r'C:\Users\jakem\FoodClassification\FOODCLASSIFICATION\model\PretrainedModel.h5')
 
 #Defining Labels for Food
 labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
@@ -17,6 +17,7 @@ labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage'
           26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
           32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
 
+#Creating lists of Fruits and Vegetables
 fruits = ['Apple', 'Banana', 'Bello Pepper', 'Chilli Pepper', 'Grapes', 'Jalepeno', 'Kiwi', 'Lemon', 'Mango', 'Orange',
           'Paprika', 'Pear', 'Pineapple', 'Pomegranate', 'Watermelon']
 
@@ -29,24 +30,52 @@ def fetch_calories(prediction) -> str:
     """
     This functions scapes the web to find the calories of a passed in image
     """
+
     try:
+        #Googling the calories in the passed in food, storing as a URL
         url = 'https://www.google.com/search?&q=calories in ' + prediction
+
+        #Retrieving the text from the URL
         req = requests.get(url).text
+
+        #Using the Beautiful Soup Web Scraper to parse the HTML and store in calories variable
         scrap = BeautifulSoup(req, 'html.parser')
         calories = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
+
         return calories
+    
     except Exception as e:
+        #Returning error message from streamlit if it cannot find
         st.error("Can't able to fetch the Calories")
         print(e)
 
+
 def processed_img(img_path):
+    """
+    This function takes in the path of an image and returns the predicted label of the image
+    using a pretrained model
+    """
+
+    #Loading image of the target size
     img = load_img(img_path, target_size=(224, 224, 3))
+
+    #Converting image to an array of values
     img = img_to_array(img)
+
+    #Normalizing the pixels
     img = img / 255
+
     img = np.expand_dims(img, [0])
+
+    #Predicting the image class using the given model
     answer = model.predict(img)
+    
+    #Storing answer as most probable prediction
     y_class = answer.argmax(axis=-1)
+
     print(y_class)
+
+    #Printing the label from the list
     y = " ".join(str(x) for x in y_class)
     y = int(y)
     res = labels[y]
@@ -55,9 +84,14 @@ def processed_img(img_path):
 
 
 def run():
-    st.title("Fruits🍍-Vegetable🍅 Classification")
+    #Defining title
+    st.title("Eat Right! Fruit and Vegetable Classification Model")
+
+    #Allowing user to upload an image of their fruit or vegetable
     img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+
     if img_file is not None:
+        #Reading in the image
         img = Image.open(img_file).resize((250, 250))
         st.image(img, use_column_width=False)
         save_image_path = './upload_images/' + img_file.name
@@ -66,13 +100,18 @@ def run():
 
         # if st.button("Predict"):
         if img_file is not None:
+            #Running prediction function on image if valid
             result = processed_img(save_image_path)
             print(result)
+
             if result in vegetables:
                 st.info('**Category : Vegetables**')
             else:
                 st.info('**Category : Fruit**')
+
             st.success("**Predicted : " + result + '**')
+
+            #Returning the calories of the predicted food label
             cal = fetch_calories(result)
             if cal:
                 st.warning('**' + cal + '(100 grams)**')
