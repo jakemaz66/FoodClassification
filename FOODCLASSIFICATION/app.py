36a68680e27357885f0b2ffef8ee5e27e072c943
jakem <mazurkiewiczj@duq.edu>
2024-03-20 12:21:43 -0400

    Got rid of tensorflow import

--- FOODCLASSIFICATION/app.py
+++ FOODCLASSIFICATION/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-import tensorflow as tf
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from keras.models import load_model
